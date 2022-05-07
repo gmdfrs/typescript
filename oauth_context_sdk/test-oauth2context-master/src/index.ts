39b0a9d1874e17cda7b0fecfe6baf545df7c6d0c
Yeser GOUMIDI <gmdfrs@gmail.com>
2022-05-07 21:30:48 +0200

    init commit

--- oauth_context_sdk/test-oauth2context-master/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Flow } from './flow';
-
-// class ClientOAuth2 extends Base {}
-// interface ClientOAuth2 extends ClientCredentials, CodeFlowWithPKCE {}
-// applyMixins(ClientOAuth2, [ClientCredentials, CodeFlowWithPKCE]);
-
-
-export default Flow;
-
--- oauth_liferay_sdk/test-liferay-sdk-js-master/src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Config{
-  oauth2context : any;
-}
-
-export type SortingDirections = 'asc'|'desc';
-
-
-  
